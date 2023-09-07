a3a35e9fab537c94a28d8ec02a3574c309440f48
Hakan <a.hakandemir23@gmail.com>
2023-09-07 14:20:28 +0300

    React-Todo-List

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import "./App.css";
+import TaskForm from "./taskForum";
+import Task from "./task";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks || []);
+  }, []);
+
+  function addTask(name) {
+    setTasks((prev) => {
+      return [...prev, { name: name, done: false }];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks((prev) => {
+      return prev.filter((taskObject, index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter((t) => t.done).length;
+  const numberTotal = tasks.length;
+
+  function getMessage() {
+    const percentage = (numberComplete / numberTotal) * 100;
+    if (percentage === 0) {
+      return "Who is Gonna carry the boats 𓊝";
+    }
+    if (percentage === 100) {
+      return "Congrats you achive failure! 🏴‍☠️";
+    }
+    return "They don't know me son 𓃮";
+  }
+
+  function renameTask(index, newName) {
+    setTasks((prev) => {
+      const newTasks = [...prev];
+      newTasks[index].name = newName;
+      return newTasks;
+    });
+  }
+
+  return (
+    <main>
+      <h1>
+        {numberComplete}/{numberTotal} Complete
+      </h1>
+      <h2>{getMessage()}</h2>
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task, index) => (
+        <Task
+          {...task}
+          onRename={(newName) => renameTask(index, newName)}
+          onTrash={() => removeTask(index)}
+          onToggle={(done) => updateTaskDone(index, done)}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/taskForum.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function TaskForm({ onAdd }) {
+  const [taskName, setTaskName] = useState("");
+  function handleSubmit(ev) {
+    ev.preventDefault();
+    onAdd(taskName);
+    setTaskName("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <button>Add</button>
+      <input
+        type="text"
+        value={taskName}
+        onChange={(ev) => setTaskName(ev.target.value)}
+        placeholder="Task"
+      />
+    </form>
+  );
+}
